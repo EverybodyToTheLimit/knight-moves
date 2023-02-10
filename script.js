7a5faf8c0ab31e636d69bbdf6a2f1b2a5fde6db0
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2023-02-10 17:38:07 +0000

    add check valid moves function, add graph edges array build function

--- script.js
+++ script.js
@@ -16,3 +16,32 @@ let buildChessBoard = () => {
         }
 return chessboard
 }
+
+let buildDependencies = (chessboard) => {
+    let dependenciesGraph = new Array
+    chessboard.forEach((element, index) => {
+        dependenciesGraph.push([element[0], element[1]])
+        dependenciesGraph[index].push(checkAllMoves(element[0], element[1]))
+    });
+    return dependenciesGraph
+
+}
+
+checkAllMoves = (x, y) => {
+    let result = new Array
+    result.push([x+1, y+2])
+    result.push([x-1, y+2])
+    result.push([x-2, y+1])
+    result.push([x-2, y-1])
+    result.push([x+1, y-2])
+    result.push([x-1, y-2])
+    result.push([x+2, y+1])
+    result.push([x+2, y-1])
+    let outcome = result.filter((item) => { 
+        if(item[0] < 8 && item[0] > 0 && item[1] < 8 && item[1] > 0) 
+        return true 
+        else return false 
+    }
+    )
+    return outcome
+}
